135d10215c186e65f80825430effbf17b5b12b96
Rijosh Easo <rijoshk@gmail.com>
2025-08-23 23:55:25 +1000

    added github workflows

--- infra/provider.tf
+++ infra/provider.tf
@@ -10,9 +10,4 @@ terraform {
 # Configure the AWS Provider
 provider "aws" {
   region = var.aws_region
-
-  assume_role {
-    role_arn     = "arn:aws:iam::616114546942:role/ooh-assumed-role"
-    session_name = "ooh-demo-session"
-  }
 }
